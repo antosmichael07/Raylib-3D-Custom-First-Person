cabc0d460ca0a680b189eb060feaa07678a4e005
Antoš Michael <antosmichael07@gmail.com>
2024-05-30 17:59:09 +0200

    made acceleration and jumping (needs fixing)

--- main.go
+++ main.go
@@ -16,22 +16,28 @@ func main() {
 
 	player := initPlayer()
 
-	bounding_box := rl.NewBoundingBox(rl.NewVector3(-1., 1.5, -1.), rl.NewVector3(1., 3.5, 1.))
+	bounding_boxes := []rl.BoundingBox{
+		rl.NewBoundingBox(rl.NewVector3(-1., 1.5, -1.), rl.NewVector3(1., 3.5, 1.)),
+		rl.NewBoundingBox(rl.NewVector3(2., 0., -1.), rl.NewVector3(4., 2, 1.)),
+		rl.NewBoundingBox(rl.NewVector3(5., -1.5, -1.), rl.NewVector3(7., .5, 1.)),
+	}
 
 	for !rl.WindowShouldClose() {
 		manageFPS(current_monitor)
 
-		updatePlayer(&player, bounding_box)
+		updatePlayer(&player, bounding_boxes)
 
 		rl.BeginDrawing()
 		{
 			rl.ClearBackground(rl.Black)
 			rl.DrawFPS(10, 10)
+
+			rl.DrawRectangle(int32(rl.GetScreenWidth())/2-5, int32(rl.GetScreenHeight())/2-5, 10, 10, rl.Fade(rl.White, .5))
 		}
 
 		rl.BeginMode3D(player.camera)
 		{
-			rl.DrawBoundingBox(bounding_box, rl.Red)
+			showBoundingBoxes(bounding_boxes, rl.Red)
 
 			rl.DrawGrid(20, 1.0)
 		}
@@ -47,5 +53,11 @@ func manageFPS(monitor int) {
 		rl.SetTargetFPS(int32(rl.GetMonitorRefreshRate(monitor)))
 	} else {
 		rl.SetTargetFPS(10)
+	}
+}
+
+func showBoundingBoxes(bounding_boxes []rl.BoundingBox, color rl.Color) {
+	for _, box := range bounding_boxes {
+		rl.DrawBoundingBox(box, color)
 	}
 }
--- player.go
+++ player.go
@@ -10,6 +10,8 @@ type Player struct {
 	speed                  float32
 	sprint_speed           float32
 	sneak_speed            float32
+	current_speed          float32
+	acceleration           float32
 	mouse_sensitivity      float32
 	zoom_mouse_sensitivity float32
 	fov                    float32
@@ -19,6 +21,10 @@ type Player struct {
 	scale                  rl.Vector3
 	crouch_scale           rl.Vector2
 	is_crouching           bool
+	y_velocity             float32
+	gravity                float32
+	jump_power             float32
+	last_key_pressed       int32
 	camera                 rl.Camera3D
 }
 
@@ -27,6 +33,8 @@ func initPlayer() Player {
 	player.speed = .1
 	player.sprint_speed = .2
 	player.sneak_speed = .05
+	player.current_speed = 0.
+	player.acceleration = .004
 	player.mouse_sensitivity = .0025
 	player.zoom_mouse_sensitivity = .0005
 	player.fov = 70.
@@ -34,28 +42,45 @@ func initPlayer() Player {
 	player.rotation = rl.NewVector2(0., 0.)
 	player.position = rl.NewVector3(4., .9, 4.)
 	player.scale = rl.NewVector3(.8, 1.8, .8)
-	player.crouch_scale = rl.NewVector2(1., 2.)
+	player.crouch_scale = rl.NewVector2(.9, 1.8)
+	player.is_crouching = false
+	player.y_velocity = 0.
+	player.gravity = .003
+	player.jump_power = .065
+	player.last_key_pressed = -1
 	player.camera = initCamera(player)
 
 	return player
 }
 
-func updatePlayer(player *Player, bounding_box rl.BoundingBox) {
-	movePlayer(player, bounding_box)
+func updatePlayer(player *Player, bounding_boxes []rl.BoundingBox) {
+	lastKeyPressedPlayer(player)
+	accelerationPlayer(player)
+	movePlayer(player, bounding_boxes)
 	rotatePlayer(player)
+	applyGravityToPlayer(player, bounding_boxes)
 	updateCameraFirstPerson(player)
 }
 
-func movePlayer(player *Player, bounding_box rl.BoundingBox) {
+func movePlayer(player *Player, bounding_boxes []rl.BoundingBox) {
 	if rl.IsKeyDown(rl.KeyLeftControl) {
 		player.scale.Y = player.crouch_scale.X
+		if !player.is_crouching {
+			player.position.Y -= player.crouch_scale.X / 2
+		}
 		player.is_crouching = true
-	} else if checkPlayerUncrouch(*player, bounding_box) {
+	} else if checkPlayerUncrouch(*player, bounding_boxes) {
 		player.scale.Y = player.crouch_scale.Y
+		if player.is_crouching {
+			player.position.Y += player.crouch_scale.X / 2
+		}
 		player.is_crouching = false
 	}
+	if rl.IsKeyDown(rl.KeySpace) && player.y_velocity == 0. && checkIfPlayerOnSurface(*player, bounding_boxes) {
+		player.y_velocity = player.jump_power
+	}
 
-	if checkCollisionsForPlayer(*player, bounding_box) {
+	if checkCollisionsForPlayer(*player, bounding_boxes) {
 		return
 	}
 
@@ -79,17 +104,24 @@ func rotatePlayer(player *Player) {
 	}
 }
 
-func checkCollisionsForPlayer(player Player, bounding_box rl.BoundingBox) bool {
+func checkCollisionsForPlayer(player Player, bounding_boxes []rl.BoundingBox) bool {
 	player.position = getPlayerPositionAfterMoving(player)
 
-	return rl.CheckCollisionBoxes(rl.NewBoundingBox(rl.NewVector3(player.position.X-player.scale.X/2, player.position.Y-player.scale.Y/2, player.position.Z-player.scale.Z/2),
-		rl.NewVector3(player.position.X+player.scale.X/2, player.position.Y+player.scale.Y/2, player.position.Z+player.scale.Z/2)), bounding_box)
+	for _, box := range bounding_boxes {
+		if rl.CheckCollisionBoxes(rl.NewBoundingBox(rl.NewVector3(player.position.X-player.scale.X/2, player.position.Y-player.scale.Y/2, player.position.Z-player.scale.Z/2),
+			rl.NewVector3(player.position.X+player.scale.X/2, player.position.Y+player.scale.Y/2, player.position.Z+player.scale.Z/2)), box) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func getPlayerPositionAfterMoving(player Player) rl.Vector3 {
 	position := player.position
+	position.Y += player.y_velocity
 
-	current_speed := player.speed
+	current_speed := player.current_speed
 	if rl.IsKeyDown(rl.KeyLeftShift) {
 		current_speed = player.sprint_speed
 	}
@@ -118,19 +150,19 @@ func getPlayerPositionAfterMoving(player Player) rl.Vector3 {
 		float32(math.Sin(float64(player.rotation.X)))*current_speed*(rl.GetFrameTime()*60),
 	)
 
-	if rl.IsKeyDown(rl.KeyW) {
+	if rl.IsKeyDown(rl.KeyW) || player.last_key_pressed == int32(rl.KeyW) {
 		position.X -= speeds.X
 		position.Z -= speeds.Y
 	}
-	if rl.IsKeyDown(rl.KeyS) {
+	if rl.IsKeyDown(rl.KeyS) || player.last_key_pressed == int32(rl.KeyS) {
 		position.X += speeds.X
 		position.Z += speeds.Y
 	}
-	if rl.IsKeyDown(rl.KeyA) {
+	if rl.IsKeyDown(rl.KeyA) || player.last_key_pressed == int32(rl.KeyA) {
 		position.Z += speeds.X
 		position.X -= speeds.Y
 	}
-	if rl.IsKeyDown(rl.KeyD) {
+	if rl.IsKeyDown(rl.KeyD) || player.last_key_pressed == int32(rl.KeyD) {
 		position.Z -= speeds.X
 		position.X += speeds.Y
 	}
@@ -138,8 +170,69 @@ func getPlayerPositionAfterMoving(player Player) rl.Vector3 {
 	return position
 }
 
-func checkPlayerUncrouch(player Player, bounding_box rl.BoundingBox) bool {
+func checkPlayerUncrouch(player Player, bounding_boxes []rl.BoundingBox) bool {
 	player.scale.Y = player.crouch_scale.Y
+	player.position.Y += player.crouch_scale.Y / 2
 
-	return !checkCollisionsForPlayer(player, bounding_box)
+	return !checkCollisionsForPlayer(player, bounding_boxes)
+}
+
+func applyGravityToPlayer(player *Player, bounding_boxes []rl.BoundingBox) {
+	player.y_velocity -= player.gravity * (rl.GetFrameTime() * 60)
+
+	if checkYCollisionsForPlayer(*player, bounding_boxes) || getPlayerPositionAfterMoving(*player).Y-(player.scale.Y/2) < 0. {
+		player.y_velocity = 0.
+		return
+	}
+
+	if checkCollisionsForPlayer(*player, bounding_boxes) {
+		player.position.Y += player.y_velocity
+	}
+}
+
+func checkYCollisionsForPlayer(player Player, bounding_boxes []rl.BoundingBox) bool {
+	player.speed = 0
+	player.sprint_speed = 0
+	player.sneak_speed = 0
+
+	return checkCollisionsForPlayer(player, bounding_boxes)
+}
+
+func checkIfPlayerOnSurface(player Player, bounding_boxes []rl.BoundingBox) bool {
+	player.position.Y -= player.gravity * (rl.GetFrameTime() * 60)
+	if checkCollisionsForPlayer(player, bounding_boxes) || player.position.Y-(player.scale.Y/2) < 0. {
+		return true
+	}
+	return false
+}
+
+func accelerationPlayer(player *Player) {
+	if player.current_speed <= player.speed && (rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyD)) {
+		player.current_speed += player.acceleration
+	}
+	if rl.IsKeyDown(rl.KeyLeftShift) && player.current_speed <= player.sprint_speed && (rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyD)) {
+		player.current_speed += player.acceleration
+	}
+	if rl.IsKeyDown(rl.KeyLeftControl) && player.current_speed <= player.sneak_speed && (rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyD)) {
+		player.current_speed += player.acceleration
+	}
+
+	if !rl.IsKeyDown(rl.KeyW) && !rl.IsKeyDown(rl.KeyS) && !rl.IsKeyDown(rl.KeyA) && !rl.IsKeyDown(rl.KeyD) && player.current_speed > 0. {
+		player.current_speed -= player.acceleration
+	}
+}
+
+func lastKeyPressedPlayer(player *Player) {
+	if rl.IsKeyDown(rl.KeyW) {
+		player.last_key_pressed = int32(rl.KeyW)
+	}
+	if rl.IsKeyDown(rl.KeyS) {
+		player.last_key_pressed = int32(rl.KeyS)
+	}
+	if rl.IsKeyDown(rl.KeyA) {
+		player.last_key_pressed = int32(rl.KeyA)
+	}
+	if rl.IsKeyDown(rl.KeyD) {
+		player.last_key_pressed = int32(rl.KeyD)
+	}
 }
